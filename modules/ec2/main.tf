417b0fde8f4d91b47a68fdf857c918f21eae206b
mohan <mohankumar130@gmail.com>
2025-03-22 19:53:25 +0530

    change in ec2 mainfile

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "pub_instance" {
   ami = var.ami_id
   instance_type = var.instance_type
   key_name = "ecs-key"
-  security_groups = [ var.pub_sg ]
+  vpc_security_group_ids = [ var.pub_sg ]
   subnet_id = var.pub_sn
   tags = {
     Name = "pub_instance"
